b456ccd11631c5652694bb8fabe4267cc181c86a
t-cbailey <t.cbaileymusic@gmail.com>
2023-08-10 11:26:43 +0100

    fixed map marker

--- src/Map.tsx
+++ src/Map.tsx
@@ -5,6 +5,8 @@ import { getEvents } from "../utils";
 import { mapProps, singleEvent } from "../customTypes";
 import { Typography } from "@mui/material";
 import { LatLngTuple } from "leaflet";
+import marker from "../public/marker.png";
+import { Icon } from "leaflet";
 
 function Map({ url, setLoading }: mapProps) {
   const [events, setEvents] = React.useState<singleEvent[]>([]);
@@ -33,7 +35,17 @@ function Map({ url, setLoading }: mapProps) {
             : event.geometry[0].coordinates;
 
           return (
-            <Marker key={event.id} position={[coords[1], coords[0]]}>
+            <Marker
+              key={event.id}
+              position={[coords[1], coords[0]]}
+              icon={
+                new Icon({
+                  iconUrl: marker,
+                  iconSize: [25, 41],
+                  iconAnchor: [12, 41],
+                })
+              }
+            >
               <Popup>
                 Name:{event.title} <br />
                 {event.description && `Description: ${event.description}.`}
